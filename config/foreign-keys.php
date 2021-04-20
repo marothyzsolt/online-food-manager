c81e820c94b7758e47c1eb8d2b6a87f35dcda591
Zsolt Marothy <zsolt.marothy@adroitgroup.io>
2021-04-20 23:47:34 +0200

    Foreign keys

--- config/foreign-keys.php
+++ config/foreign-keys.php
@@ -9,31 +9,48 @@
  *   ]
  */
 
+use App\Models\Allergen;
+use App\Models\Category;
+use App\Models\Currency;
+use App\Models\Item;
+use App\Models\Media;
+use App\Models\Menu;
+use App\Models\Restaurant;
+use App\Models\User;
+
 return [
     'menu_item' => [
-        'menu_id' => \App\Models\Menu::class,
-        'item_id' => \App\Models\Item::class,
+        'menu_id' => Menu::class,
+        'item_id' => Item::class,
     ],
     'category_item' => [
-        'category_id' => \App\Models\Category::class,
-        'item_id' => \App\Models\Item::class,
+        'category_id' => Category::class,
+        'item_id' => Item::class,
     ],
     'menu_restaurant' => [
-        'restaurant_id' => \App\Models\Category::class,
-        'menu_id' => \App\Models\Menu::class,
+        'restaurant_id' => Category::class,
+        'menu_id' => Menu::class,
     ],
     'allergen_item' => [
-        'allergen_id' => \App\Models\Allergen::class,
-        'item_id' => \App\Models\Item::class,
+        'allergen_id' => Allergen::class,
+        'item_id' => Item::class,
     ],
     'item_prices' => [
-        'currency_id' => \App\Models\Currency::class,
-        'item_id' => \App\Models\Item::class,
+        'currency_id' => Currency::class,
+        'item_id' => Item::class,
     ],
     'menus' => [
-        'media_id' => \App\Models\Media::class,
+        'media_id' => Media::class,
+        'restaurant_id' => Restaurant::class,
     ],
     'opening_hours' => [
-        'restaurant_id' => \App\Models\Restaurant::class,
+        'restaurant_id' => Restaurant::class,
+    ],
+    'restaurants' => [
+        'user_id' => User::class,
+    ],
+    'item_media' => [
+        'item_id' => Item::class,
+        'media_id' => Media::class,
     ],
 ];
